bb2cd5a83426a131b6599c2e1023af8404f312bf
Burim Cakolli <Burim.cakolli@siemens.com>
2018-12-10 16:39:03 +0100

    Init Code-Example with moon and stars

---
+++ Singleton-Pattern-Moon-Example/Singleton-Pattern-Moon-Example/Moon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern_Moon_Example
+{
+    public class Moon
+    {
+        private static Moon moon;
+
+        private Moon()
+        {
+            //Here constructor code for Moon creation
+        }
+
+        public static Moon GetInstance()
+        {
+            if (moon == null)
+            {
+                moon = new Moon();
+            }
+            return moon;
+        }
+    }
+}
---
+++ Singleton-Pattern-Moon-Example/Singleton-Pattern-Moon-Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern_Moon_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Singleton---");
+            Moon obj1 = Moon.GetInstance();
+            Moon obj2 = Moon.GetInstance();
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+
+            Console.WriteLine("---Non Singleton---");
+            Star obj3 = new Star();
+            Star obj4 = new Star();
+            Console.WriteLine(obj3.GetHashCode());
+            Console.WriteLine(obj4.GetHashCode());
+
+            //Press CTRL+F5 to run
+        }
+    }
+}
